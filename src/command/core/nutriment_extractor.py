1ccfc7f4cdc93054627aea62b53f1807ef671984
Olivier <olivier.bialorucki@labom2iformation.fr>
2026-09-22 13:28:41 +0200

    FIX erreur après refacto

--- src/command/core/nutriment_extractor.py
+++ src/command/core/nutriment_extractor.py
@@ -7,6 +7,8 @@ import json
 from src.command.core.execution_timer import ExecutionTimer
 
 class NutrimentExtractor:
+    TARGET_NUTRIMENT = ['energy', 'sugars', 'saturated-fat', 'salt', 'sodium', 'fiber', 'proteins', 'fruits-vegetables-legumes', 'fat']
+
     def __init__(self):
         self.batch_size = int(os.getenv("EXTRACT_BATCH_SIZE", "10000"))
         self.source_parquet = os.getenv("EXTRACT_OUTPUT_PARQUET_FILE")
@@ -14,9 +16,6 @@ class NutrimentExtractor:
 
         columns = os.getenv("EXTRACT_SELECTED_COLUMNS")
         self.selected_columns = columns.split(",") if columns else None
-
-        nutriments_columns = os.getenv("NUTRIMENT_COLUMNS")
-        self.nutriments_columns = nutriments_columns.split(",") if columns else None
 
         self.timer = ExecutionTimer()
 
@@ -80,7 +79,7 @@ class NutrimentExtractor:
 
                 df_base_chunk = batch.select(base_columns).to_pandas()
 
-                df_nutrients_chunk = self.process_nutriment_batch(batch, self.nutriments_columns)
+                df_nutrients_chunk = self.process_nutriment_batch(batch, self.TARGET_NUTRIMENT)
 
                 df_combined_chunk = pd.concat([df_base_chunk, df_nutrients_chunk], axis=1)
                 all_chunks.append(df_combined_chunk)
